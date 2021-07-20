fe8083a5ef21beb0a312f290372a52d46aa5d740
dianhsu <xudian.cn@gmail.com>
2021-07-20 14:18:41 +0800

    fix

--- deploy/sentry_release.sh
+++ deploy/sentry_release.sh
@@ -10,9 +10,9 @@ if [ ! -z $USE_SENTRY ] && [ $USE_SENTRY == '1' ]; then
 
 # create new release according to `VERSION`
 docker run --rm -v $(pwd):/work getsentry/sentry-cli \
-   sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases -o onlinejudge -p onlinejudgefe new $VERSION
+   sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases --org onlinejudge -p onlinejudgefe new $VERSION
 
 # upload js and source_maps
 docker run --rm -v $(pwd):/work getsentry/sentry-cli \
-   sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases -o onlinejudge -p onlinejudgefe files $VERSION upload-sourcemaps ./dist/static/js
+   sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases --org onlinejudge -p onlinejudgefe files $VERSION upload-sourcemaps ./dist/static/js
 fi
